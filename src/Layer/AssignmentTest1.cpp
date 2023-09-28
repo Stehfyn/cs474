40756464026ae781f3bc57ab319663a4458c974a
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2023-09-27 18:21:13 -0700

    Assignment Test 1

---
+++ src/Layer/AssignmentTest1.cpp
@@ -0,0 +1,20 @@
+#include "..\cs474.pch.h"
+#include "AssignmentTest1.h"
+namespace cs474 {
+AssignmentTest1::AssignmentTest1() {
+}
+AssignmentTest1::~AssignmentTest1() {
+}
+
+void AssignmentTest1::OnAttach() {
+}
+
+void AssignmentTest1::OnDetach() {
+}
+
+void AssignmentTest1::OnUpdate(float ts) {
+}
+
+void AssignmentTest1::OnUIRender() {
+}
+}
