9545ab0294b60bc2540926d27bcf8d1d7de07944
DuckieOnQuacks <jojopaschke@gmail.com>
2023-09-27 18:45:33 -0700

    Update AssignmentTest2.cpp

--- src/Layer/AssignmentTest2.cpp
+++ src/Layer/AssignmentTest2.cpp
@@ -9,3 +9,19 @@ cs474::AssignmentTest2::AssignmentTest2()
 cs474::AssignmentTest2::~AssignmentTest2()
 {
 }
+
+void cs474::AssignmentTest2::OnAttach()
+{
+}
+
+void cs474::AssignmentTest2::OnDetach()
+{
+}
+
+void cs474::AssignmentTest2::OnUpdate(float ts)
+{
+}
+
+void cs474::AssignmentTest2::OnUIRender()
+{
+}
