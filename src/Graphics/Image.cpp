ac9fee15d4d74e766bb250af7f5f75d59fcc7cd3
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2023-09-28 18:38:21 -0700

    ImageRegistry (epic)

--- src/Graphics/Image.cpp
+++ src/Graphics/Image.cpp
@@ -8,15 +8,16 @@ enum Status {
 };
 
 Image::Image(const std::vector<uint8_t>& data, uint32_t width, uint32_t height, uint32_t channels)
-	: m_RendererID(Status::Invalid), m_Width(width), m_Height(height) {
+	: m_RendererID(Status::Invalid), m_Width(width), m_Height(height), m_Data(data) {
 	m_RendererID = CreateTexture((unsigned char*)data.data(), width, height, channels);
 	Unbind();
 }
 
 Image::Image(unsigned char* data, uint32_t width, uint32_t height, uint32_t channels) 
-	: m_RendererID(Status::Invalid), m_Width(width), m_Height(height) {
+	: m_RendererID(Status::Invalid), m_Width(width), m_Height(height), m_Data(width * height * channels) {
 	m_RendererID = CreateTexture(data, width, height, channels);
 	Unbind();
+	std::memcpy(m_Data.data(), data, m_Data.size());
 }
 
 Image::~Image() {
@@ -31,13 +32,11 @@ void Image::Unbind(void) const {
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-size_t Image::GetWidth() const
-{
+size_t Image::GetWidth() const {
 	return m_Width;
 }
 
-size_t Image::GetHeight() const
-{
+size_t Image::GetHeight() const {
 	return m_Height;
 }
 
@@ -45,9 +44,28 @@ GLuint Image::GetRendererID() const{
 	return m_RendererID;
 }
 
+const std::vector<uint8_t>& Image::GetRawData() const {
+	return m_Data;
+}
+
 // Something to test around and keep in mind:
 // https://github.com/ocornut/imgui/issues/3523
 GLuint Image::CreateTexture(unsigned char* data, uint32_t width, uint32_t height, uint32_t channels) {
+	// No support for variance between internal and represented format
+	int format;
+	switch (channels) {
+	case 1: {
+		format = GL_LUMINANCE;
+		break;
+	}
+	case 4: {
+		format = GL_RGBA;
+		break;
+	}
+	default:
+		throw std::runtime_error("Unsupported number of channels!");
+	}
+
 	GLuint tex;
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
@@ -64,13 +82,97 @@ GLuint Image::CreateTexture(unsigned char* data, uint32_t width, uint32_t height
 	#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
 		glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
 	#endif
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	return tex;
 }
 
 void Image::DeleteTexture(GLuint& renderer_id) {
 	glDeleteTextures(1, &renderer_id);
+}
+ImageRegistry::ImageRegistry() {
+}
+ImageRegistry::~ImageRegistry() {
+}
+bool ImageRegistry::AddFileList(const std::string& key, const utils::FileList& list) {
+	m_Registry.emplace(key, std::make_pair(list, TextureRegistry_{}));
+	return true; //TODO: Add logic for success/failure etc.
+}
+bool ImageRegistry::AddTexture(const std::string& key, const std::string& sub_key, const Texture& tex) {
+	auto it = m_Registry.find(key);
+
+	if (it != m_Registry.end()) {
+		const auto& file_list = it->second.first; 
+
+		if (file_list.empty())
+			return false; // false if file_list is empty (somehow)
+
+		auto& texture_registry = it->second.second; 
+		auto result = texture_registry.emplace(sub_key, tex); 
+		return result.second; // result.second is true if insertion is successful, false if sub_key already exists
+	}
+
+	return false; // Return false if key does not exist in the registry
+}
+bool ImageRegistry::LoadDefaultTexturesFromFileList(void) {
+	for (const auto& kv : m_Registry) {
+		const auto& file_list = kv.second.first;
+		for (const auto& file : file_list) {
+			// default load texture
+			if (!file.has_extension()) {
+				throw std::runtime_error("Attempt to load an image without an extension!");
+			}
+
+			int desired_channels = 4;
+
+			if (std::string(file.extension()) == ".pgm") 
+				desired_channels = 1;
+			
+			int image_width = 0;
+			int image_height = 0;
+
+			const auto& data = utils::SlurpFile(file);
+			if (data.has_value()) {
+				unsigned char* image_data = stbi_load_from_memory((unsigned char*)data.value().data(), data.value().size(), &image_width, &image_height, NULL, desired_channels);
+
+				if (image_data == NULL) {
+					emscripten_log(EM_LOG_CONSOLE, "didnt work lol");
+				}
+
+				this->AddTexture(kv.first, std::string(file.extension()), graphics::make_texture(image_data, image_width, image_height, desired_channels));
+
+				stbi_image_free(image_data);
+			}
+		}
+	}
+
+
+	return false;
+}
+std::vector<std::string> ImageRegistry::GetKeys() const {
+	std::vector<std::string> keys;
+	for (const auto& kv : m_Registry) {
+		keys.push_back(kv.first);
+	}
+	return keys;
+}
+std::optional<utils::FileList> ImageRegistry::GetFileList(const std::string& key) const {
+	auto it = m_Registry.find(key);
+	if (it != m_Registry.end()) {
+		return it->second.first;
+	}
+	return std::nullopt;
+}
+std::optional<Texture> ImageRegistry::GetTexture(const std::string& key, const std::string& sub_key) const {
+	auto it = m_Registry.find(key);
+	if (it != m_Registry.end()) {
+		const auto& texture_registry = it->second.second;
+		auto tex_it = texture_registry.find(sub_key);
+		if (tex_it != texture_registry.end()) {
+			return tex_it->second;
+		}
+	}
+	return std::nullopt;
 }
 }
 }
--- src/Graphics/Image.h
+++ src/Graphics/Image.h
@@ -13,6 +13,7 @@ public:
 	size_t GetWidth() const;
 	size_t GetHeight() const;
 	GLuint GetRendererID() const;
+	const std::vector<uint8_t>& GetRawData() const;
 
 private:
 	GLuint CreateTexture(unsigned char* data, uint32_t width, uint32_t height,
@@ -23,6 +24,7 @@ private:
 	size_t m_Width;
 	size_t m_Height;
 	GLuint m_RendererID;
+	std::vector<uint8_t> m_Data;
 };
 
 using Texture = std::shared_ptr<Image>;
@@ -32,8 +34,40 @@ Texture make_texture(Args&&... args) {
 	return std::make_shared<Image>(std::forward<Args>(args)...);
 }
 
-using TextureRegistry = std::vector<Texture>;
+// ImageRegistry:
+//		   (key)
+//		{ "lenna" => 
+//				{ {assets/images/raw/lenna.gif, assets/images/pgm/lenna.pgm},
+//					 (sub_key)
+//				  { "lenna_gif" =>
+//						Texture },
+//					"lenna_pgm" =>
+//						Texture }
+//				}
+//		}, ...
 
+class ImageRegistry {
+	using TextureRegistry_ = std::unordered_map<std::string, Texture>;
+	using ImageData = std::pair<utils::FileList, TextureRegistry_>;
+
+public:
+	ImageRegistry();
+	~ImageRegistry();
+
+public:
+	bool AddFileList(const std::string& key, const utils::FileList& list);
+	bool AddTexture(const std::string& key, const std::string& sub_key, const Texture& tex);
+	bool LoadDefaultTexturesFromFileList(void);
+
+	std::vector<std::string> GetKeys() const;
+	std::optional<utils::FileList> GetFileList(const std::string& key) const;
+	std::optional<Texture> GetTexture(const std::string& key, const std::string& sub_key) const;
+
+private:
+	std::unordered_map<std::string, ImageData> m_Registry;
+};
+
+using TextureRegistry = std::vector<Texture>;
 }
 }
 
